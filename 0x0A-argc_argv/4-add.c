e5f4dceff7197697eda4f86ef68ff56ee60f56f1
edidiong etim <etimedidiong2019@gmail.com>
2023-10-22 18:42:34 -0500

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,69 +1,39 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - this program adds two numbers
-(* a blank line
-*@argc: this is the quantity of parameters
-*@argv: the parameter to put in the program.
-* Description: this program adds two numbers)?
-* Return: return 0 or 1
- */
+ * main - program that adds positive numbers.
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int result;
+	int i, suma = 0, res = 0;
+	char c[] = "Error", *find_letter;
 
-	result = 0;
-
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (i = 1; i < argc; i++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			find_letter = argv[i];
+			while (*find_letter != 0)
 			{
-				printf("Error\n");
-				return (1);
+				if (*find_letter < 47 || *find_letter > 57)
+				{
+					printf("%s\n", c);
+					return (1);
+				}
+				find_letter++;
 			}
-
+			res = atoi(argv[i]);
+			suma += res;
 		}
-
-		result += _atoi(argv[i]);
-
+		printf("%d\n", suma);
 	}
-
-	printf("%d\n", result);
-
+	else
+		printf("%d\n", 0);
 	return (0);
-}
-/**
- * _atoi - this function converts a string to integer
-(* a blank line
-*@s: the string for convert to integer
-* Description: this funtion converts a string to integer)?
-(* section header: the header of this function is holberton.h)*
-* Return: this funtion returns the result
-*/
-int _atoi(char *s)
-{
-	int counter, sign;
-	unsigned int number;
-
-	sign = 1;
-	counter = 0;
-	number = 0;
-
-
-	while ((s[counter] < '0' || s[counter] > '9') && (s[counter] != '\0'))
-	{
-		if (s[counter] == '-')
-			sign *= -1;
-		counter++;
-	}
-	while ((s[counter] >= '0') && (s[counter] <= '9'))
-	{
-		number = number * 10 + (s[counter] - '0');
-		counter++;
-	}
-	return (number * sign);
 }
